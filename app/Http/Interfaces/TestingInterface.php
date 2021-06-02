20695230c5f3687f8f2d035ad644b321c960056e
mostafa-zayed <mostafa.zayed9990@gmail.com>
2021-06-02 22:28:54 +0200

    update Testing Module and finsh it

--- app/Http/Interfaces/TestingInterface.php
+++ app/Http/Interfaces/TestingInterface.php
@@ -4,5 +4,11 @@ namespace App\Http\Interfaces;
 
 interface TestingInterface
 {
-    public function index();
+    public function indexTesting();
+    public function showTesting($id);
+    public function createTesting();
+    public function storeTesting($request);
+    public function editTesting($id);
+    public function updateTesting($request,$id);
+    public function destroyTesting($id);
 }
--- app/Http/Repositories/TestingRepository.php
+++ app/Http/Repositories/TestingRepository.php
@@ -6,8 +6,38 @@ use App\Http\Interfaces\TestingInterface;
 
 class TestingRepository implements TestingInterface
 {
-    public function index()
+    public function indexTesting()
     {
-        return 'testing';
+        // TODO: Implement indexTesting() method.
+    }
+
+    public function showTesting($id)
+    {
+        // TODO: Implement showTesting() method.
+    }
+
+    public function createTesting()
+    {
+        // TODO: Implement createTesting() method.
+    }
+
+    public function storeTesting($request)
+    {
+        // TODO: Implement storeTesting() method.
+    }
+
+    public function editTesting($id)
+    {
+        // TODO: Implement editTesting() method.
+    }
+
+    public function updateTesting($request, $id)
+    {
+        // TODO: Implement updateTesting() method.
+    }
+
+    public function destroyTesting($id)
+    {
+        // TODO: Implement destroyTesting() method.
     }
 }
