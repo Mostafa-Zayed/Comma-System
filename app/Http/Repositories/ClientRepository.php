94f88d9b8c52f73dc18dd4a55e9ec92a2fdb9b85
mostafa-zayed <mostafa.zayed9990@gmail.com>
2021-06-04 18:36:57 +0200

    adding migration file

---
+++ app/Http/Repositories/ClientRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Http\Interfaces\ClientInterface;
+
+class ClientRepository implements ClientInterface
+{
+
+    public function indexClient()
+    {
+        // TODO: Implement indexClient() method.
+    }
+
+    public function showClient($id)
+    {
+        // TODO: Implement showClient() method.
+    }
+
+    public function createClient()
+    {
+        // TODO: Implement createClient() method.
+    }
+
+    public function storeClient($request)
+    {
+        // TODO: Implement storeClient() method.
+    }
+
+    public function editClient($id)
+    {
+        // TODO: Implement editClient() method.
+    }
+
+    public function updateClient($request, $id)
+    {
+        // TODO: Implement updateClient() method.
+    }
+
+    public function destroyClient($id)
+    {
+        // TODO: Implement destroyClient() method.
+    }
+}
---
+++ database/migrations/2021_06_04_162051_create_clients_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',70);
+            $table->string('email',100)->unique();
+            $table->string('ssn')->unique();
+            $table->string('phone',25)->nullable();
+            $table->string('job',255);
+            $table->enum('status',['on','of'])->default('on');
+            $table->foreignId('employee_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            // RelationShip Mapping
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('set null')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
