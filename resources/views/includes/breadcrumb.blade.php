85fabe8bbad3da8f21d6dc455b1aefcfedb86c74
mostafa-zayed <mostafa.zayed9990@gmail.com>
2021-06-08 15:07:01 +0200

    add files model

--- resources/views/includes/breadcrumb.blade.php
+++ resources/views/includes/breadcrumb.blade.php
@@ -1,7 +1,7 @@
 <nav class="breadcrumb-two" aria-label="breadcrumb">
     <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="{{route('index')}}">Home</a></li>
-        <li class="breadcrumb-item active"><a href="{{route($models.'.'.$method)}}">{{ucfirst($models)}}</a></li>
+        <li class="breadcrumb-item active"><a href="{{route($models.'.index')}}">{{ucfirst($models)}}</a></li>
         <li class="breadcrumb-item" aria-current="page">{{ucfirst($method)}}</li>
     </ol>
 </nav>
