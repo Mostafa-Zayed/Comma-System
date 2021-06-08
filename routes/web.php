e052d21658ff7e6c67c140159ecedc39a1352ff5
mostafa-zayed <mostafa.zayed9990@gmail.com>
2021-06-08 14:32:31 +0200

    add route , controller , migration , repo , interface , view

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\RoomController;
+use App\Http\Controllers\TypeController;
 
 /** Employees Urls */
 Route::resource('employees', EmployeeController::class)->where(['employee' => '[0-9]+']);
@@ -18,5 +19,5 @@ Route::get('/', [MainController::class, 'index'])->name('index');
 /**  Rooms Urls */
 Route::resource('rooms',RoomController::class)->where(['room' => '[0-9]+']);
 
-
-
+/**  Types Urls */
+Route::resource('types',TypeController::class)->where(['type' => '[0-9]+']);
