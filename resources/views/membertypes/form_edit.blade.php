28e64d1c8fe3764e2da42b1a7e8b43ff7db22d71
mostafa-zayed <mostafa.zayed9990@gmail.com>
2021-07-26 18:30:27 +0200

    add edit adn update and delete Membet Type

--- resources/views/membertypes/form_edit.blade.php
+++ resources/views/membertypes/form_edit.blade.php
@@ -1,10 +1,11 @@
-<form action="{{route('member-types.store')}}" method="post" enctype="multipart/form-data">
+<form action="{{route('member-types.update',['member_type' => $row])}}" method="post" enctype="multipart/form-data">
     @csrf
+    {{method_field('PUT')}}
     <div class="form-row">
         <div class="col-md-12 mb-4">
             @php $input = 'name'; @endphp
             <label for="e_mail">{{ucwords($input)}}</label>
-            <input type="text" class="form-control" id="{{str_replace(' ','',$input)}}" name="{{str_replace(' ','',$input)}}" value="@isset($row->{str_replace(' ','',$input)}) {{ $row->{str_replace(' ','',$input)} }}@endisset {{old(str_replace(' ','',$input))}}">
+            <input type="text" class="form-control" id="{{str_replace(' ','',$input)}}" name="{{str_replace(' ','',$input)}}" value="@isset($row->{$input}) {{$row->{$input} }}@endisset">
             @error(str_replace(' ','',$input))
             <div class="invalid-feedback" style="display: block;">
                 {{$message}}
@@ -14,7 +15,7 @@
         <div class="col-md-12 mb-4">
             @php $input = 'days'; @endphp
             <label for="e_mail">{{ucwords($input)}}</label>
-            <input type="number" class="form-control" id="{{str_replace(' ','',$input)}}" name="{{str_replace(' ','',$input)}}" value="@isset($row->{str_replace(' ','',$input)}) {{ $row->{str_replace(' ','',$input)} }}@endisset {{old(str_replace(' ','',$input))}}">
+            <input type="number" class="form-control" id="{{str_replace(' ','',$input)}}" name="{{str_replace(' ','',$input)}}" value="@isset($row->{$input}){{$row->$input}}@endisset">
             @error(str_replace(' ','',$input))
             <div class="invalid-feedback" style="display: block;">
                 {{$message}}
@@ -24,7 +25,7 @@
         <div class="col-md-12 mb-4">
             @php $input = 'price'; @endphp
             <label for="e_mail">{{ucwords($input)}}</label>
-            <input type="number" class="form-control" id="{{str_replace(' ','',$input)}}" name="{{str_replace(' ','',$input)}}" value="@isset($row->{str_replace(' ','',$input)}) {{ $row->{str_replace(' ','',$input)} }}@endisset {{old(str_replace(' ','',$input))}}">
+            <input type="number" class="form-control" id="{{str_replace(' ','',$input)}}" name="{{str_replace(' ','',$input)}}" value="@isset($row->{$input}){{$row->$input}}@endisset">
             @error(str_replace(' ','',$input))
             <div class="invalid-feedback" style="display: block;">
                 {{$message}}
@@ -38,19 +39,19 @@
             <label for="{{$input}}">{{ucwords($input)}}</label>
             <div id="select_menu" class="form-group mb-4">
                 <select class="custom-select" name="{{$input}}">
-                        <option value="on">On</option>
-                        <option value="off">Off</option>
+                    <option value="on" @if($row->{$input} == 'on') selected @endif>On</option>
+                    <option value="off" @if($row->{$input} == 'off') selected @endif>Off</option>
                 </select>
                 @error($input)
-                    <div class="invalid-feedback">
-                        Please Select the field
-                    </div>
+                <div class="invalid-feedback">
+                    Please Select the field
+                </div>
                 @enderror
             </div>
         </div>
     </div>
     <div class="form-row float-right">
-        @php $input = 'create'; @endphp
+        @php $input = 'edit'; @endphp
         <input class="btn btn-primary mb-4 mt-2 text-right" type="submit" value="{{$method}}" name="{{$input}}">
     </div>
 </form>
